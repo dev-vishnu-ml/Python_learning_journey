3329329d58dfb926541ed04711c67bb6b9d5c7d0
dev-vishnu-ml <vishnusharma0531@gmail.com>
2026-08-10 21:05:53 +0530

    add list slicing practice

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
-# 1. lists -> 
+# 1. lists -> slcining 
 numbers = [99,45,67,54,90,91]
 numbers[0] = 89 # item assignment 
 print(numbers[0]) #updated item value
 print(type(numbers)) # type is list
+
---
+++ list_slicing.py
@@ -0,0 +1,5 @@
+# Slicing operations on list
+list1 = [50,90,80,43,29,"vishnu",3.14]
+print(list1[5:6]) # i want to print vishnu
+print(list1[:5]) # print the integer numbers by default value is start from 0
+print(list1[5:]) # length of list is = n
