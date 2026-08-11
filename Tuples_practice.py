0bf1587bde9bdda56625e0d51ff3cf2f5207a689
dev-vishnu-ml <vishnusharma0531@gmail.com>
2026-08-11 11:42:01 +0530

    tuple practice code

---
+++ Tuples_practice.py
@@ -0,0 +1,13 @@
+# tuples in python -> creating tuples with paranthesis ()
+ 
+tup = (1,6,77,89,34,"vishnu",5.9)
+print(f"values of tuples: {tup}") 
+print(f"calculate the length of tuples: {len(tup)}")
+print(f"print the type of tuple: {type(tup)}")
+
+# Slicing operation on tuple
+print(f"Slicing operation : {tup[3:]}")
+
+#we can not create only 1 tuple value without comma(,)
+tuple_1 = (1,) 
+print(type(tuple_1))
