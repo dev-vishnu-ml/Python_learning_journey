5398ac350e1f17ee6c981eb54112c93f8765d49f
dev-vishnu-ml <vishnusharma0531@gmail.com>
2026-08-11 12:05:27 +0530

    added methods of tuples practice

---
+++ Methods_of_tuples.py
@@ -0,0 +1,7 @@
+# Method of tuples : 1.index(return 1st occurance)
+#2. count(return total of tuples value)
+tupl = (1,2,3,1,5,6,3,7,3)
+print(type(tupl))
+print(tupl.index(3)) # answer = 2 return 1st index of value
+
+print(tupl.count(3)) # answer = 3 count total tuple
