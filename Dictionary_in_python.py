dd91b347cca417741d263e69b1cbefb8f414ed57
dev-vishnu-ml <vishnusharma0531@gmail.com>
2026-08-11 15:25:46 +0530

    dictionary in python practice code

---
+++ Dictionary_in_python.py
@@ -0,0 +1,13 @@
+# dictionary is key:value pairs 
+# the dictionaries are mutable acces the value using key 
+student_info = {
+    "name":"vishnu sharma",
+    "age": 18,
+    "subject":["math","computer Science"],
+    "cgpa": 9.2
+}
+# dictionaries are mutable it is a example
+student_info["name"] = "Tarun"
+student_info["cgpa"] = 9.6
+
+print(student_info)
