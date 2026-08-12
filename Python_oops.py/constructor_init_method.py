bf2608ad566e3310ae1438560ae0f244ad1eb30e
dev-vishnu-ml <vishnusharma0531@gmail.com>
2026-08-12 19:25:56 +0530

    constructor basic concepet

---
+++ Python_oops.py/constructor_init_method.py
@@ -0,0 +1,6 @@
+# constructor Method: 
+class abc:
+    def __init__(self):
+        print("constructor was called ...")
+
+stud1 = abc() # these peranthesis () means we call the construcor in our class
