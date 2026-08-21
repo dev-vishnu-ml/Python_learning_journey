f482a2d305f25778959fd4dd6f952282423c2fbb
dev-vishnu-ml <vishnusharma0531@gmail.com>
2026-08-21 18:37:08 +0530

    Encapsulation access and update the private attributes in python using the getters and setters functuion

---
+++ Python_oops.py/Encapsulation_private attribute.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self,name,balance):
+        self.name = name
+        self.__balance = balance #private attribute
+
+# basically getters and setters is use to get and update the private variables
+
+    def getattribute(self): #getters function
+        return self.__balance
+    
+    def setattribute(self,new_name,newbalance): #setters function
+        self.name = new_name
+        self.__balance = newbalance
+
+acc1 = BankAccount("rahul kumar",100_000)
+print(acc1.name,acc1.getattribute())
+acc1.setattribute("shardha",200_000)
+print(acc1.name,acc1.getattribute())
